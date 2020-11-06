6714567fc37e82dad742961d0fc3ff388d2c5203
zeyadShapan <zeyadshapan2004@gmail.com>
2020-11-06 08:22:48 +0200

    Fixed page not responsive

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
-document.addEventListener("DOMContentLoaded",function(){window.addEventListener("load",function(){window.onscroll=function(e){this.scrollY>100?document.querySelector(".navbar").classList.add("sticky"):document.querySelector(".navbar").classList.remove("sticky"),this.scrollY>500?document.querySelector(".scroll-up-btn").classList.add("show"):document.querySelector(".scroll-up-btn").classList.remove("show")}}),document.querySelector(".scroll-up-btn").onclick=function(){$("html").animate({scrollTop:0}),$("html").css("scrollBehavior","auto")},document.querySelector(".navbar .menu li a").onclick=function(){$("html").css("scrollBehavior","smooth")},document.querySelector(".menu-btn").onclick=function(){$(".navbar .menu").toggleClass("active"),$(".menu-btn i").toggleClass("active")};new Typed(".typing",{strings:["Backend developer","Full-Stack developer","Freelancer","Growing a Startup"],typeSpeed:100,backSpeed:60,loop:!0}),new Typed(".typing-2",{strings:["Backend developer","Full-Stack developer","Freelancer","Growing a Startup"],typeSpeed:100,backSpeed:60,loop:!0})});
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
