6ffd52f84f0d5fc6890f2ab7d15d44fb96522ef5
zeyadShapan <zeyadshapan2004@gmail.com>
2020-12-11 22:29:17 +0200

    Added Trade4All and modified skills and about

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web developer", "Mobile developer", "Owning a startup"],
+        strings: ["Web developer", "Mobile developer", "Freelancer", "Orgachat founder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web developer", "Mobile developer", "Owning a startup"],
+        strings: ["Web developer", "Mobile developer", "Freelancer", "Orgachat founder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
