24f6976fd39ce39f2f95c2fc3d42a46e1c540507
zeyadShapan <zeyadshapan2004@gmail.com>
2020-12-16 17:21:55 +0200

    Update porfolio style and colors

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web developer", "Mobile developer", "Freelancer", "Orgachat founder"],
+        strings: ["MERN full stack developer", "Mobile developer", "Founder & CEO"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web developer", "Mobile developer", "Freelancer", "Orgachat founder"],
+        strings: ["MERN full stack developer", "Mobile developer", "Founder & CEO"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
